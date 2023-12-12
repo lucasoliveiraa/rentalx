f3f5b5c296d9a933c002f6bf58716f48dfa7768c
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-12-12 17:44:52 -0300

    ✨Create Sentry app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,37 +1,62 @@
-import "reflect-metadata";
+import cors from "cors";
+import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import createConnection from "@shared/infra/typeorm";
-
-import "@shared/container";
-import { AppError } from "@shared/errors/AppError";
-import { router } from "@shared/infra/http/routes";
-import swaggerFile from "../../../swagger.json";
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
-createConnection();
+import "reflect-metadata";
+import "@shared/container";
+
+import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+dotenv.config();
+
 const app = express();
+
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
+
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
+app.use(Sentry.Handlers.errorHandler());
 
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
-  },
-);
+  }
 
-export { app }
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
