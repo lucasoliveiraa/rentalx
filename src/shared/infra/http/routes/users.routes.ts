774c37efc1593ecb27fd9a8f39390819043a6bbd
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-12-12 14:51:43 -0300

    ✨Create routes

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
 import multer from "multer";
+
 import uploadConfig from "@config/upload";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -18,7 +21,9 @@ usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
   uploadAvatar.single("avatar"),
-  updateUserAvatarController.handle,
+  updateUserAvatarController.handle
 );
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
